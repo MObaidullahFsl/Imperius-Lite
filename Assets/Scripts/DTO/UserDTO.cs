cdf474917e649b40bb170b0f9e53de84321fe1f4
Umair Maqsood <142876620+Umairmaqsood00@users.noreply.github.com>
2025-11-30 20:32:18 +0500

    updated userdto

---
+++ Assets/Scripts/DTO/UserDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace Imperius.Data
+{
+    public class UserDTO
+    {
+        public string username {get; set;}
+        public string password {get; set;}  
+        public string email {get; set;}
+
+    }
+}
