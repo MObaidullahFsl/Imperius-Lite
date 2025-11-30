9f058ec76b745fd10e64b723323b84d6287ff3ec
Umair Maqsood <142876620+Umairmaqsood00@users.noreply.github.com>
2025-11-30 20:30:28 +0500

    update match.dto

--- Assets/Scripts/DTO/MatchDTO.cs
+++ Assets/Scripts/DTO/MatchDTO.cs
@@ -1,12 +1,24 @@
 using System;
+using System.Collections.Generic;
 using Imperius.Logic;
 
 namespace Imperius.Data
 {
     [Serializable]
     public class MatchDTO
-    {   public int MatchId;
-        public int RoomId;
-        public int EnemyId;
+    {
+   public string RoomId;
+    public string MatchId;
+    public string EnemyName;
+
+    public string PlayerName; // name
+
+    public List<Character> PlayerTeam;
+    public List<Character> EnemyTeam;
+
+    public Deck PlayerDeck;
+    public Deck EnemyDeck;
+
+    public string ActivePlayer;
     }
 }
